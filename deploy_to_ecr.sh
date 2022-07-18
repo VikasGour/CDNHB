d441a66405eb04ab91a5bfcc4f1839847e658542
VikasGour <32245623+VikasGour@users.noreply.github.com>
2022-07-18 22:26:38 +0800

    Create deploy_to_ecr.sh

---
+++ deploy_to_ecr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+#image_file=$(find . -name "*tar")
+ 
+# load image into local (jump point) docker
+#docker image load -i "$image_file"
+ 
+# get image id of loaded docker image
+image_id=$1
+ 
+# login to ecr (jump point needs the appropriate IAM permissions via ec2 instance role/access keys)
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "784190227945.dkr.ecr.us-west-2.amazonaws.com"
+ 
+# push loaded docker image to ecr
+docker tag "nodejs-jenkins-CIFORNHB-${image_id}" "784190227945.dkr.ecr.us-west-2.amazonaws.com/nhb:nodejs-jenkins-CIFORNHB-${image_id}"
+docker push "784190227945.dkr.ecr.us-west-2.amazonaws.com/nhb:nodejs-jenkins-CIFORNHB-${image_id}"
+ 
+# update ecs service with new image (jump point needs appropriate iam permissions)
+#aws ecs update-service --cluster "$ecs_cluster_name" --service "$ecs_service_name" --force-new-deployment
